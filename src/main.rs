9d7dd5e196b601efab6ce12309ffd74e5506073a
Bastian Gruber <gruberbastian@me.com>
2019-03-05 17:19:40 +0100

    Add logs

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,7 @@ struct GitHubRedirect {
 }
 
 async fn get_github_url() -> String {
-    println!("HEERE");
+
     let github_uri = "https://github.com/login/oauth/authorize?scope=user:email&client_id=";
     format!("{}{:?}", github_uri, env::var("GH_BASIC_CLIENT_ID").unwrap())
 }
@@ -40,7 +40,7 @@ fn debug_store(ctx: RequestContext<()>) -> FutureObj<Response> {
 
 fn main() {
     let mut app = tide::App::new(());
-
+    println!("HEERE");
     database::establish_connection();
 
     app.middleware(debug_store);
