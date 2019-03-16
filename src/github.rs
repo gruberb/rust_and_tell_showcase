904ca9e3b9b685f5728a7cabb12a9bfadf8c28da
Bastian Gruber <gruberbastian@me.com>
2019-03-16 13:48:14 +0100

    Use proper query params when getting github emails

--- src/github.rs
+++ src/github.rs
@@ -1,12 +1,7 @@
-use std::collections::HashMap;
-
 pub fn get_github_emails(access_token: &str) -> Result<String, reqwest::Error>  {
-    let mut params = HashMap::new();
-    params.insert("access_token", access_token);
-
     let client = reqwest::Client::new();
     let mut res = client.get("https://api.github.com/user/emails")
-        .form(&params)
+        .query(&[("access_token", access_token)])
         .send()
         .expect("Failed to send request");
 
